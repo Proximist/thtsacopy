6146498fc512a4808897dbcda74859aa447b809f
Proximist <103359694+Proximist@users.noreply.github.com>
2024-11-23 21:20:44 +0530

    Delete metadata.ts

--- metadata.ts
+++
@@ -1,7 +0,0 @@
-// app/metadata.ts
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Home - CATS Dashboard',
-  description: 'A simple Telegram mini app using Next.js and Prisma',
-};
